fa106761153b54dc57a55ac858aacf793467a087
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2023-09-06 12:06:18 +0530

    Array

--- Array.java
+++ Array.java
@@ -2,6 +2,9 @@ public class Array {
     public static void main(String args[]){
         int num[]={1,2,3,4};
         num[1]=6;
+
+        int nums[]=new int[4];
+
         System.out.println(num[1]);
     }
 }
